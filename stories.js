7f4268c18fd753e4e3329fb63b8f65fbe690c7c3
Harry Sayers <harrysayers12345@gmail.com>
2025-08-03 19:46:14 +0100

    added test story

---
+++ stories.js
@@ -0,0 +1,8 @@
+export const stories = [{
+  id: "a-test-story",
+  date: "5th July 2025", 
+  title: "New Story Title",
+  description: "Story description...",
+  image: "/a-test-story/preview.jpg",
+  url: "/a-test-story",
+}];
