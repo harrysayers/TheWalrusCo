5062cd70970201130df641c731cc95fd5315fa79
Harry Sayers <harrysayers12345@gmail.com>
2025-08-03 20:04:39 +0100

    added test story

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
         trailingSlash: 'always',
-        redirects: {
-            // This catches any URL without trailing slash and adds it
-            '/[...path]': {
-            status: 301,
-            destination: '/[...path]/'
-            }
-        },
         site: 'https://www.thewalrusco.net',
 });
